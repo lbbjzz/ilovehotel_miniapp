040f51db437da3ba5f0c43857783a6af9c2f5cef
lbbjzz <lbbjzz@gmail.com>
2021-03-02 21:33:39 +0800

    Update: HomePage improvements

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -1,6 +1,6 @@
 import React, {Component} from 'react'
-// import { View, Text } from '@tarojs/components'
-import {AtTabBar, AtSearchBar} from 'taro-ui'
+import {View, Text, Swiper, SwiperItem} from '@tarojs/components'
+import {AtTabBar, AtSearchBar, AtNavBar} from 'taro-ui'
 
 // import "taro-ui/dist/style/components/button.scss" // 按需引入
 import 'taro-ui/dist/style/index.scss' //全局引入
@@ -23,6 +23,7 @@ export default class Index extends Component {
     this.state = {
       value: '',
       current: 0,
+      title: '首页'
     }
   }
 
@@ -41,19 +42,48 @@ export default class Index extends Component {
   render() {
     return (
       <div>
+       {/*头部*/}
+        <AtNavBar
+          className={"home-navbar"}
+          color='#ececec'
+          title={this.state.title}
+        />
+        {/*搜索*/}
         <AtSearchBar
           value={this.state.value}
           onChange={this.onChange.bind(this)}
         />
+        {/*轮播图*/}
+        <Swiper
+          className='test-h'
+          indicatorColor='#999'
+          indicatorActiveColor='#333'
+          vertical={false}
+          circular
+          indicatorDots
+          autoplay={true}>
+          <SwiperItem>
+            <View className='demo-text-1'>焦</View>
+          </SwiperItem>
+          <SwiperItem>
+            <View className='demo-text-2'>猪</View>
+          </SwiperItem>
+          <SwiperItem>
+            <View className='demo-text-3'>猪</View>
+          </SwiperItem>
+        </Swiper>
+        {/*底部*/}
         <AtTabBar
-          current={this.state.current}
+          fixed
+          backgroundColor='#ececec'
           tabList={
             [
-              {title: '待办事项', iconType: 'bullet-list'},
-              {title: '拍照', iconType: 'camera'},
-              {title: '文件夹', iconType: 'folder'}
+              {title: '首页', iconType: 'home'},
+              {title: '订单', iconType: 'bullet-list'},
+              {title: '个人中心', iconType: 'user'}
             ]
           }
+          current={this.state.current}
           onClick={this.handleClick.bind(this)}
         />
       </div>

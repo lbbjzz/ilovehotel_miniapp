40769b006d04ff6d8fc33a53bcdb220d31314bb9
JZZ <ldld99@qq.com>
2021-03-02 16:30:23 +0800

    Update: HomePage

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -1,32 +1,62 @@
-import React, { Component } from 'react'
-import { View, Text } from '@tarojs/components'
-import { AtButton } from 'taro-ui'
+import React, {Component} from 'react'
+// import { View, Text } from '@tarojs/components'
+import {AtTabBar, AtSearchBar} from 'taro-ui'
 
-import "taro-ui/dist/style/components/button.scss" // 按需引入
+// import "taro-ui/dist/style/components/button.scss" // 按需引入
+import 'taro-ui/dist/style/index.scss' //全局引入
 import './index.scss'
 
 export default class Index extends Component {
 
-  componentWillMount () { }
+  //生命周期
+  // componentWillMount () { }
+  //
+  // componentDidMount () { }
+  //
+  // componentWillUnmount () { }
+  //
+  // componentDidShow () { }
+  //
+  // componentDidHide () { }
+  constructor() {
+    super(...arguments)
+    this.state = {
+      value: '',
+      current: 0,
+    }
+  }
 
-  componentDidMount () { }
+  handleClick(value) {
+    this.setState({
+      current: value
+    })
+  }
 
-  componentWillUnmount () { }
+  onChange(value) {
+    this.setState({
+      value: value
+    })
+  }
 
-  componentDidShow () { }
-
-  componentDidHide () { }
-
-  render () {
+  render() {
     return (
-      <View className='index'>
-        <Text>Hello world!</Text>
-        <AtButton type='primary'>I need Taro UI</AtButton>
-        <Text>Taro UI 支持 Vue 了吗？</Text>
-        <AtButton type='primary' circle={true}>支持</AtButton>
-        <Text>共建？</Text>
-        <AtButton type='secondary' circle={true}>来</AtButton>
-      </View>
+      <div>
+        <AtSearchBar
+          value={this.state.value}
+          onChange={this.onChange.bind(this)}
+        />
+        <AtTabBar
+          current={this.state.current}
+          tabList={
+            [
+              {title: '待办事项', iconType: 'bullet-list', text: 'new'},
+              {title: '拍照', iconType: 'camera'},
+              {title: '文件夹', iconType: 'folder', text: '100', max: 99}
+            ]
+          }
+          onClick={this.handleClick.bind(this)}
+        />
+      </div>
     )
   }
 }
